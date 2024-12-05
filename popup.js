e372cae225ba755c437e783f8ec4a97485219cfe
jemish <jemish.sakariya@yudizsolutions.com>
2024-12-05 13:55:19 +0530

    Add Prettier configuration and update popup files for consistency and error handling

--- popup.js
+++ popup.js
@@ -2,21 +2,21 @@ function parseTime(timeString) {
   if (!timeString) return null;
 
   // Handle "5h 43m +" format
-  if (timeString.includes("h") && timeString.includes("m")) {
-    const parts = timeString.split(" ");
-    const hours = parseInt(parts[0].replace("h", ""), 10);
-    const minutes = parseInt(parts[1].replace("m", ""), 10);
+  if (timeString.includes('h') && timeString.includes('m')) {
+    const parts = timeString.split(' ');
+    const hours = parseInt(parts[0].replace('h', ''), 10);
+    const minutes = parseInt(parts[1].replace('m', ''), 10);
 
     return { effectiveHours: hours, effectiveMinutes: minutes };
   }
 
   // Handle "4:31:51 PM" format
-  const [time, modifier] = timeString.split(" ");
-  let [hours, minutes, seconds] = time.split(":").map(Number);
+  const [time, modifier] = timeString.split(' ');
+  let [hours, minutes, seconds] = time.split(':').map(Number);
 
-  if (modifier === "PM" && hours !== 12) {
+  if (modifier === 'PM' && hours !== 12) {
     hours += 12;
-  } else if (modifier === "AM" && hours === 12) {
+  } else if (modifier === 'AM' && hours === 12) {
     hours = 0;
   }
 
@@ -27,10 +27,8 @@ function calculateTimeDifference(currentTime, lastTime) {
   const currentTimeParsed = parseTime(currentTime);
   const lastTimeParsed = parseTime(lastTime);
 
-  let remainingHours =
-    7 - currentTimeParsed.effectiveHours + lastTimeParsed.lastHours;
-  let remainingMin =
-    60 - currentTimeParsed.effectiveMinutes + lastTimeParsed.lastMinutes;
+  let remainingHours = 7 - currentTimeParsed.effectiveHours + lastTimeParsed.lastHours;
+  let remainingMin = 60 - currentTimeParsed.effectiveMinutes + lastTimeParsed.lastMinutes;
 
   if (remainingMin >= 60) {
     remainingHours++;
@@ -38,12 +36,12 @@ function calculateTimeDifference(currentTime, lastTime) {
   }
 
   remainingHours = remainingHours > 12 ? remainingHours - 12 : remainingHours;
-  remainingMin = remainingMin.toString().padStart(2, "0");
+  remainingMin = remainingMin.toString().padStart(2, '0');
 
   return `${remainingHours}:${remainingMin}`;
 }
 
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   const tab = tabs[0];
 
   if (tab) {
@@ -52,7 +50,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         target: { tabId: tab.id },
         func: extractTimes,
       },
-      async (results) => {
+      async results => {
         // if (chrome.runtime.lastError) {
         //   const messageElement = document.querySelector(".message");
         //   messageElement.textContent = "Please First Load the PunchIn Time";
@@ -60,48 +58,54 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         // }
 
         const result = await results[0].result;
-        const { currentTime, lastTime } = result;
+        const { currentTime, lastTime, error } = result;
+
+        if (error) {
+          const messageElement = document.querySelector('.message');
+          messageElement.textContent = error;
+          messageElement.style.color = '#ff584d';
+          return;
+        }
 
         if (currentTime && lastTime) {
           const timeDifference = calculateTimeDifference(currentTime, lastTime);
 
           chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: (timeDifference) => {
-              const currentTimeElement = document.querySelector(
-                ".d-flex .pie-percent + div span"
-              );
+            func: timeDifference => {
+              const currentTimeElement = document.querySelector('.d-flex .pie-percent + div span');
               const currentTimeElementParent = currentTimeElement.parentNode;
 
               if (currentTimeElementParent) {
-                currentTimeElementParent.style.display = "flex";
-                currentTimeElementParent.style.alignItems = "center";
-                currentTimeElementParent.style.textAlign = "center";
-                currentTimeElementParent.style.gap = "20px";
+                currentTimeElementParent.style.display = 'flex';
+                currentTimeElementParent.style.alignItems = 'center';
+                currentTimeElementParent.style.textAlign = 'center';
+                currentTimeElementParent.style.gap = '20px';
 
-                const existingDisplay =
-                  currentTimeElementParent.querySelector(".time-display");
+                const themeMode = localStorage.getItem('ThemeMode');
+                const existingDisplay = currentTimeElementParent.querySelector('.time-display');
+
                 if (existingDisplay) {
                   existingDisplay.textContent = `Time: ${timeDifference}`;
+                  const getDisplayElement = document.querySelector('.time-display');
+                  getDisplayElement.style.backgroundColor = themeMode === 'light' ? '#f5f5f5' : '#263042';
                 } else {
-                  const displayElement = document.createElement("div");
-                  displayElement.classList.add("time-display");
+                  const displayElement = document.createElement('div');
+                  displayElement.classList.add('time-display');
                   displayElement.textContent = `Time: ${timeDifference}`;
-                  displayElement.style.padding = "10px";
-                  const themeMode = localStorage.getItem("ThemeMode");
-                  displayElement.style.backgroundColor =
-                    themeMode === "light" ? "#f5f5f5" : "#263042";
-                  currentTimeElementParent.insertBefore(
-                    displayElement,
-                    currentTimeElement.nextSibling
-                  );
+                  displayElement.style.padding = '10px';
+                  displayElement.style.backgroundColor = themeMode === 'light' ? '#f5f5f5' : '#263042';
+                  currentTimeElementParent.insertBefore(displayElement, currentTimeElement.nextSibling);
                 }
               }
             },
             args: [timeDifference],
           });
+
+          const messageElement = document.querySelector('.message');
+          messageElement.textContent = 'Your Time Shown Next To Effective Hour';
         } else {
-          console.log("Keka Time Extention: No times found");
+          console.log('Keka Time Extention: No times found');
         }
       }
     );
@@ -109,30 +113,31 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 });
 
 function extractTimes() {
-  const currentTimeElement = document.querySelector(
-    ".d-flex .pie-percent + div span"
-  );
-  const currentTime = currentTimeElement
-    ? currentTimeElement.textContent.trim()
-    : null;
+  const currentTimeElement = document.querySelector('.d-flex .pie-percent + div span');
+  const currentTime = currentTimeElement ? currentTimeElement.textContent.trim() : null;
 
   if (currentTimeElement) {
     currentTimeElement.click();
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       setTimeout(() => {
-        const parentElement = document.querySelector(
-          ".d-flex.mt-10:last-child"
-        );
-        const lastTimeElement = parentElement
-          ? parentElement.querySelector(".ki-arrow-forward.ki-green + span")
-          : null;
-        const lastTime = lastTimeElement
-          ? lastTimeElement.textContent.trim()
-          : null;
+        const missingPunchElement = document.querySelector('.d-flex.mt-10:last-child .d-flex.align-items-center.ml-8 span:nth-child(2)');
+
+        if (missingPunchElement.textContent !== 'MISSING') {
+          resolve({
+            currentTime,
+            lastTime: null,
+            error: 'Please Wait for PunchIn Time',
+          });
+          return;
+        }
+
+        const parentElement = document.querySelector('.d-flex.mt-10:last-child');
+        const lastTimeElement = parentElement ? parentElement.querySelector('.ki-arrow-forward.ki-green + span') : null;
+        const lastTime = lastTimeElement ? lastTimeElement.textContent.trim() : null;
 
         // console.log("Extracted Times:", { currentTime, lastTime });
         resolve({ currentTime, lastTime });
-      }, 500);
+      }, 300);
     });
   }
 
